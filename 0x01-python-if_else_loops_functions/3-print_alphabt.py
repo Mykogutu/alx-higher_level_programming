828d1ec68a69080635219e493876264533a72a71
Mykogutu <ogutu47@gmail.com>
2023-01-05 10:42:28 -0800

    corrections

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for i in range(97, 123):
     if i == ord('e') or i == ord('q'):
         continue
     else:
-        print(chr(i), end="")
+        print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0,100):
-    if i < 10:
-        print(f"0{i}", end=", ")
-    elif i == 99:
-        print(i)
+    if i == 99:
+        print("{}".format(i))
     else:
-        print(i, end=", ")
+        print("{:02}".format(i), end=", ")
