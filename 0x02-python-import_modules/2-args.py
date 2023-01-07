0e53d757e8ec3565ad6114fd5405166ac9d0b0d4
Mykogutu <ogutu47@gmail.com>
2023-01-06 23:23:56 -0800

    sys.argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    import sys
-    size = len(sys.argv)
-    if size <= 1:
+    size = len(sys.argv) - 1
+    if size == 0:
         print("0 arguments.")
+    elif size == 1:
+        print("1 arguments:")
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(size))
     for i in range(1, size):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, argv[i]))
