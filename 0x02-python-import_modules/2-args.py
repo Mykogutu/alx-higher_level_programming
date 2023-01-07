c6df12c6ef6d9d9812832645a67118459ffb632b
Mykogutu <ogutu47@gmail.com>
2023-01-07 02:02:46 -0800

    corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 from sys import argv
 
 if __name__ == "__main__":
-    size = len(sys.argv) - 1
+    size = len(argv) - 1
     if size == 0:
-        print("0 arguments.")
+        print("0 arguments")
     elif size == 1:
-        print("1 arguments:")
+        print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(size))
-    for i in range(1, size):
-        print("{}: {}".format(i, argv[i]))
+        for i in range(size + 1):
+            print("{}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
     size = len(sys.argv)
     total = 0
 
