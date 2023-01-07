bb1203c665eeb4ab80d59138d3baa4b7aec466c6
Mykogutu <ogutu47@gmail.com>
2023-01-07 02:11:03 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 from sys import argv
 
 if __name__ == "__main__":
-    size = len(argv) - 1
+    size = len(argv)
     if size == 0:
         print("0 arguments.")
     elif size == 1:
@@ -10,5 +10,5 @@ if __name__ == "__main__":
         print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(size))
-        for i in range(size + 1):
+        for i in range(1, size):
             print("{}: {}".format(i, argv[i]))
